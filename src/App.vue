2d63b856ddbac3dfd52afb79c7425ef5766d610c
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-05-31 16:42:19 +0200

    [UPD] BONUS DONE

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppJumbo from './components/AppJumbo.vue';
 
 export default {
   name:'MyApp',
   components:{
     AppTitle,
+    AppJumbo,
   },
 }; 
 </script>
 
 <template>
   <AppTitle />
+
+  <AppJumbo />
 </template>
 
 <style scoped>
---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    nome: 'AppJumbo',
+}
+
+</script>
+
+<template>
+    <h2>JUMBOTRON</h2>
+
+</template>
+
+<style>
+
+</style>
